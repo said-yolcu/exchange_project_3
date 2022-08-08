c97e8d9d0e31bdf7350c1d32dbbe2770dce19458
said-yolcu <saidyolcu.saidyolcu@gmail.com>
2022-08-08 18:29:09 +0300

    Get input from the user and make a request to api

--- exchange_service.js
+++ exchange_service.js
@@ -1,30 +1,59 @@
-var http = require('https')
+import { request } from 'https'
+console.log('B 2')
+let exchange_parities = new Map()
 
-function send_request(base, to) {
-    var options = {
-        'method': 'GET'
-        , 'hostname': 'api.collectapi.com'
-        , 'port': null
-        , 'path': `/economy/exchange?int=1&to=${to}&base=${base}`
+function send_request(base, to, api_key) {
+
+    let map_key = `${to}_${base}`
+    console.log('B 5')
+
+    // If the key already exists in the map, return it
+    if (exchange_parities.has(map_key)) {
+        return new Promise((resolve, reject) => {
+            resolve(exchange_parities.get(map_key))
+        })
     }
 
-    return new Promise((resolve,reject) => {
-        var req = http.request(options, res => {
+    // Else request the parity from the api
+    var options = {
+        "method": "GET",
+        "hostname": "api.collectapi.com",
+        "port": null,
+        "path": `/economy/exchange?int=1&to=${to}&base=${base}`,
+        "headers": {
+            "content-type": "application/json",
+            "authorization": api_key
+        }
+    }
+
+    return new Promise((resolve, reject) => {
+        var req = request(options, res => {
             let chunks = []
-    
+
+            console.log('B 21')
             res.on('data', chunk => {
                 chunks.push(chunk)
             })
-    
+
+            console.log('B 26')
             res.on('end', () => {
                 let body = Buffer.concat(chunks)
-                resolve(body.toString())
+                console.log(body.toString())
+                exchange_parities.set(map_key
+                    , body.toString()
+                        .match(/rate/gi)[0])
+                resolve(exchange_parities.get(map_key))
+                console.log(body.toString())
             })
 
-            res.on('error', e => reject(e))
+            res.on('error', e => {
+                console.log('Exchange service error')
+                reject(e)
+            })
         })
-    }).finally(() => req.end())
-
+        req.end()
+    })
+        .catch(e => console.log('Catched error' + e))
 }
 
-module.exports = { send_request }
+export default { send_request }
--- main.js
+++ main.js
@@ -1,27 +1,46 @@
-const exchange_service = require('./exchange_service.js')
-const read_line = require('readline').createInterface({
-    input: process.stdin
-    , output: process.stdout
+import exchange_service from './exchange_service.js'
+import rl from 'readline-promise'
+const readline = rl.default
+const read_line = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: true
 })
+import { api_key } from './api_key.js'
 
-function serve(service) {
+async function serve(service) {
     service = service.toString().replace(/\s+|\n/g, '')
     switch (service) {
         case 'exchange':
             // Do case exchange
+            /*
             function choose_base() {
-                read_line.question('Choose your base currency'
-                    , base)
-                return base
+                return new Promise((resolve, reject)=>{
+                    resolve(read_line.question('Choose your base currency'))
+                })
             }
-            function choose_to() {
-                read_line.question('To which currency would you \
-                like to convert ')
-                return to
+            function choose_to(base) {
+                return new Promise((resolve, reject)=>{
+                    resolve(read_line.question('Choose your to currency'))
+                })
             }
-            let base = choose_base()
-            let to = choose_to()
-            exchange_service.send_request().then(res => console.log(res))
+            */
+            console.log(27)
+            let base = await read_line.questionAsync('Choose your base')
+            console.log(29)
+            let to = await read_line.questionAsync('Choose your to')
+            console.log(31)
+            let amount = await read_line.questionAsync('Enter the amount')
+            amount = Number(amount)
+
+            function inform_user(rate) {
+                console.log(`The exchange rate is ${rate}.
+And the ${amount} ${base} is equivalent to ${amount * rate}`)
+            }
+
+            exchange_service
+                .send_request(base, to, api_key)
+                .then(inform_user)
                 .catch(e => console.error('Warning! Error: ' + e))
             break
         default:
@@ -35,3 +54,5 @@ function choose_service() {
     read_line.question('Which service would you like to use?\n\
 Write one of these: exchange, ...', serve)
 }
+
+choose_service()
