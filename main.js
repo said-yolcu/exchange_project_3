ff062e23f1aae80b7ac3bc85bc00f6fc447b6950
said-yolcu <saidyolcu.saidyolcu@gmail.com>
2022-08-08 15:00:03 +0300

    Gitignore

--- main.js
+++ main.js
@@ -1,12 +1,37 @@
 const exchange_service = require('./exchange_service.js')
-const read_line= require('readline').createInterface({
+const read_line = require('readline').createInterface({
     input: process.stdin
     , output: process.stdout
 })
 
-function serve(service){
-
+function serve(service) {
+    service = service.toString().replace(/\s+|\n/g, '')
+    switch (service) {
+        case 'exchange':
+            // Do case exchange
+            function choose_base() {
+                read_line.question('Choose your base currency'
+                    , base)
+                return base
+            }
+            function choose_to() {
+                read_line.question('To which currency would you \
+                like to convert ')
+                return to
+            }
+            let base = choose_base()
+            let to = choose_to()
+            exchange_service.send_request().then(res => console.log(res))
+                .catch(e => console.error('Warning! Error: ' + e))
+            break
+        default:
+            console.log('You made an invalid choice')
+            choose_service()
+    }
 }
 
-read_line.question('Which service would you like to use?\n\
+
+function choose_service() {
+    read_line.question('Which service would you like to use?\n\
 Write one of these: exchange, ...', serve)
+}
